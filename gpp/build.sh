8d6cd6e10b66ad941dddb078edb5abdc6c8eb354
cn6u9 <cnbug@live.cn>
2025-05-10 15:52:36 +0800

    Delete gpp directory

--- gpp/build.sh
+++
@@ -1,8 +0,0 @@
-#go install github.com/wailsapp/wails/v2/cmd/wails@latest
-#wails build -m -trimpath -tags webkit2_41,with_quic
-
-npm install
-npm run build
-cd ..
-export NODE_OPTIONS="--max-old-space-size=4096"
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 wails build -tags with_gvisor --skipbindings
--- gpp/server/core/config.go
+++
@@ -1,8 +0,0 @@
-package core
-
-type Peer struct {
-	Protocol string `json:"protocol"`
-	Port     uint16 `json:"port"`
-	Addr     string `json:"addr"`
-	UUID     string `json:"uuid"`
-}
